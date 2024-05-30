cf0f509fbb51cef57ce6926ea9e5fea68fad2fc5
YichuanZhangthemostHANDSOMEone <Yichuan.Zhang@my.jcu.edu.au>
2024-05-30 16:55:46 +0800

    Determine score with function

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,15 +2,25 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    result = "Invalid score"
-elif score > 90:
-    result = "Excellent"
-elif score > 50:
-    result = "Passable"
-else:
-    result = "Bad"
-print(result)
+def main():
+    score = random.randint(0, 100)
+    result = determine_score(score)
+    print(f"Your result is {result}")
+
+
+def determine_score(score):
+    if score < 0 or score > 100:
+        result = "Invalid score"
+    elif score > 90:
+        result = "Excellent"
+    elif score > 50:
+        result = "Passable"
+    else:
+        result = "Bad"
+    return result
+
+
+main()
