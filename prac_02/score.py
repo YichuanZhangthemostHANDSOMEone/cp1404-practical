455206be71af5a3f04104d55beee5bd6b4ead48b
YichuanZhangthemostHANDSOMEone <Yichuan.Zhang@my.jcu.edu.au>
2024-05-30 16:41:40 +0800

    Change name to score

---
+++ prac_02/score.py
@@ -0,0 +1,16 @@
+"""
+CP1404/CP5632 - Practical
+Broken program to determine score status
+"""
+
+
+score = float(input("Enter score: "))
+if score < 0 or score > 100:
+    result = "Invalid score"
+elif score > 90:
+    result = "Excellent"
+elif score > 50:
+    result = "Passable"
+else:
+    result = "Bad"
+print(result)
