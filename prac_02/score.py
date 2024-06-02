9f472d0ec605436dceedbba643acd4b6c394c9fe
YichuanZhangthemostHANDSOMEone <Yichuan.Zhang@my.jcu.edu.au>
2024-06-02 15:17:35 +0800

    Add docstring

--- prac_02/score.py
+++ prac_02/score.py
@@ -6,12 +6,14 @@ import random
 
 
 def main():
+    """main function"""
     score = random.randint(0, 100)
     result = determine_score(score)
     print(f"Your result is {result}")
 
 
 def determine_score(score):
+    """determine what level the inputted score is"""
     if score < 0 or score > 100:
         result = "Invalid score"
     elif score > 90:
