dc2d876214e9eb3302ef0c362138eb4537c278bc
YichuanZhangthemostHANDSOMEone <Yichuan.Zhang@my.jcu.edu.au>
2024-06-16 22:54:28 +0800

    Eliminate duplicate number generation

--- prac_04/quik_picks.py
+++ prac_04/quik_picks.py
@@ -4,6 +4,8 @@ input_number = int(input("How many quik picks:"))
 for i in range(input_number):
     for n in range(5):
         random_number = random.randint(1, 45)
+        while random_number in random_numbers:
+            random_number = random.randint(1, 45)
         random_numbers.append(random_number)
 for m in range(0, len(random_numbers), 5):
     numbers = random_numbers[m:m+5]
